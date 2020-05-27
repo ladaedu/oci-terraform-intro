cc2c85d16d9e55cab0c3a02e7ccf35ace0ab3bcf
root <root@azurit-06.azuritkovo.local>
2020-05-27 11:16:59 +0200

    Add terratests

--- web-server/terratest/terraform_oci_test.go
+++ web-server/terratest/terraform_oci_test.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"testing"
 	"time"
+	"os/exec"
 )
 
 const (
@@ -69,6 +70,10 @@ func runSubtests(t *testing.T) {
 	t.Run("netstatNginx", netstatNginx)
 	t.Run("curlWebServer", curlWebServer)
 	t.Run("checkVpn", checkVpn)
+	t.Run("checkCompute", checkCompute)
+	t.Run("checkLoadBalancer", checkLoadBalancer)
+	t.Run("sshLoadBalancer", sshLoadBalancer)
+	t.Run("curlLoadBalancer", curlLoadBalancer)
 }
 
 func sshBastion(t *testing.T) {
@@ -85,6 +90,18 @@ func netstatNginx(t *testing.T) {
 
 func curlWebServer(t *testing.T) {
 	curlService(t, "nginx", "", "80", "200")
+}
+
+func curlLoadBalancer(t *testing.T){
+	curlServiceLB(t, "nginx", "", "80", "200")
+}
+
+func sshLoadBalancer(t *testing.T){
+	err := ssh.CheckSshConnectionE(t, lbHost(t))  
+	if err == nil {
+		t.Fatalf("error in calling vcn: %s", err.Error())
+	}
+
 }
 
 func checkVpn(t *testing.T) {
@@ -121,12 +138,122 @@ func checkVpn(t *testing.T) {
 	}
 }
 
+func checkCompute(t *testing.T) {
+	// client
+	config := common.CustomProfileConfigProvider("", "CzechEdu")
+	c, _ := core.NewComputeClientWithConfigurationProvider(config)
+	// c, _ := core.NewVirtualNetworkClientWithConfigurationProvider(common.DefaultConfigProvider())
+
+	// request
+	request := core.GetInstanceRequest{}
+	instanceId := sanitizeInstanceId(t)
+	request.InstanceId = &instanceId
+
+	//t.Fatalf(instanceId)
+
+	// response
+	response, err := c.GetInstance(context.Background(), request)
+
+	if err != nil {
+		t.Fatalf("error in calling instance: %s", err.Error())
+	}
+
+	// assertions
+	expected := "webServer0-default"
+	actual := response.Instance.DisplayName
+
+	if expected != *actual {
+		t.Fatalf("wrong instance display name: expected %q, got %q", expected, *actual)
+	}
+
+	expected = "VM.Standard2.1"
+	actual = response.Shape
+
+	if expected != *actual {
+		t.Fatalf("wrong shape: expected %q, got %q", expected, *actual)
+	}
+}
+
+func checkLoadBalancer(t *testing.T) {
+	// client
+	config := common.CustomProfileConfigProvider("", "CzechEdu")
+	c, _ := core.NewVirtualNetworkClientWithConfigurationProvider(config)
+
+	// request
+	request := core.GetSubnetRequest{}
+	loadBalancerId := sanitizeLoadBalancerId(t)
+	request.SubnetId = &loadBalancerId
+
+	// response
+	response, err := c.GetSubnet(context.Background(), request)
+
+	if err != nil {
+		t.Fatalf("error in calling vcn: %s", err.Error())
+	}
+
+	// assertions
+	expected := "Loadbalancer Subnet-default"
+	actual := response.Subnet.DisplayName
+
+	if expected != *actual {
+		t.Fatalf("wrong subnet display name: expected %q, got %q", expected, *actual)
+	}
+
+	expected = "10.0.200.0/28"
+	actual = response.Subnet.CidrBlock
+
+	if expected != *actual {
+		t.Fatalf("wrong cidr block: expected %q, got %q", expected, *actual)
+	}
+	
+	secListId := response.Subnet.SecurityListIds[0]
+	secListRequest := core.GetSecurityListRequest{}
+	secListRequest.SecurityListId = &secListId
+
+	newresponse, newerr := c.GetSecurityList(context.Background(), secListRequest)
+
+	if newerr != nil {
+		t.Fatalf("error in calling seclist: %s", newerr.Error())
+	}
+
+	outexpected := 1
+	outactual := len(newresponse.SecurityList.EgressSecurityRules)
+
+	if outexpected != outactual {
+		t.Fatalf("wrong number of egress rules: expected %q, got %q", outexpected, outactual)
+	}
+
+	inexpected := 2
+	inactual := len(newresponse.SecurityList.IngressSecurityRules)
+
+	if inexpected != inactual {
+		t.Fatalf("wrong number of egress rules: expected %q, got %q", inexpected, inactual)
+	}
+}
+
+
+
+func sanitizeInstanceId(t *testing.T) string {
+	raw:= terraform.Output(t, options, "InstanceId")
+	return strings.Split(raw, "\"")[1]
+}
+
+func sanitizeLoadBalancerId(t *testing.T) string {
+	raw:= terraform.Output(t, options, "LoadBalancerId")
+	return strings.Split(raw, "\"")[1]
+}
+
+
 func sanitizedVcnId(t *testing.T) string {
 	raw := terraform.Output(t, options, "VcnID")
 	return strings.Split(raw, "\"")[1]
 }
 
 // ~~~~~~~~~~~~~~~~ Helper functions ~~~~~~~~~~~~~~~~
+func lbHost(t *testing.T) ssh.Host {
+	lbIP := terraform.OutputList(t, options, "lb_ip")[0]
+	return sshHost(t, lbIP)
+}
 
 func bastionHost(t *testing.T) ssh.Host {
 	bastionIP := terraform.OutputList(t, options, "BastionPublicIP")[0]
@@ -171,13 +298,31 @@ func curlService(t *testing.T, serviceName string, path string, port string, ret
 		}
 	}
 }
+func curlServiceLB(t *testing.T, serviceName string, path string, port string, returnCode string) {
+	//loadBHost := lbHost(t)
+	host := loadBalancerIPs(t)
+	url := fmt.Sprintf("http://%s:%s%s", host, port, path)
+	curl := exec.Command("curl", url)
+	_, err := curl.Output()
+	if err != nil {
+		t.Fatalf("error curl to %s", url)
+		return
+	}
+}
+
 
 func curl(host string, port string, path string) string {
+	//return fmt.Sprintf("curl http://%s:%s%s", host, port, path)
 	return fmt.Sprintf("curl -s -o /dev/null -w '%%{http_code}' http://%s:%s%s", host, port, path)
 }
 
+
 func webServerIPs(t *testing.T) []string {
 	return terraform.OutputList(t, options, "WebServerPrivateIPs")
+}
+
+func loadBalancerIPs(t *testing.T) string {
+	return terraform.OutputList(t, options, "lb_ip")[0]
 }
 
 func jumpSsh(t *testing.T, command string, expected string, retryAssert bool) string {
