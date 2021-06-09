40cc21560a98eeddca610be2101b42ea04d1e1bd
Adrian Macak <adrian.ado.macak@gmail.com>
2021-06-10 00:01:13 +0200

    Working exercises.

--- web-server/variables.tf
+++ web-server/variables.tf
@@ -17,13 +17,13 @@ variable "ssh_private_key" {
 }
 
 variable "region" {
-  default = "us-phoenix-1"
+  default = "eu-frankfurt-1"
 }
 
 /* Availability domain can be 0, 1 or 2 - use the one that has free resources */
-#variable "availability_domain" {
-#  default = 1
-#}
+variable "availability_domain" {
+  default = "NoND:EU-FRANKFURT-1-AD-2"
+}
 
 variable "TestServerShape" {
   default = "VM.Standard2.1"
@@ -38,8 +38,7 @@ variable "InstanceImageOCID" {
   # TIP: the variable map can be (re-)defined also in env-vars file.
   default = {
     // Oracle-Linux-7.6-2019.02.20-0
-    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaacss7qgb6vhojblgcklnmcbchhei6wgqisqmdciu3l4spmroipghq"
-    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaarruepdlahln5fah4lvm7tsf4was3wdx75vfs6vljdke65imbqnhq"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaavz6p7tyrczcwd5uvq6x2wqkbwcrjjbuohbjomtzv32k5bq24rsha"
   }
 }
 
