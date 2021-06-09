51f5ae8f858e3f65ec09d6656a688ee1b38bd936
Vít Kotačka <vit.kotacka@oracle.com>
2021-06-09 11:09:36 +0200

    Necessary fixes to run tests and avoid problems with quota

--- web-server/outputs.tf
+++ web-server/outputs.tf
@@ -1,8 +1,8 @@
 # Output the private and public IPs of the instance
 
-# output "WebServerPrivateIPs" {
-#   value = [oci_core_instance.WebServer.*.private_ip]
-# }
+output "WebServerPrivateIPs" {
+  value = [oci_core_instance.WebServer.*.private_ip]
+}
 
 # output "WebServerPublicIPs" {
 #   value = ["${oci_core_instance.WebServer.*.public_ip}"]
@@ -16,9 +16,9 @@
 #   value = [oci_core_subnet.PrivateSubnet.*.subnet_domain_name]
 # }
 
-# output "BastionPublicIP" {
-#   value = [oci_core_instance.Bastion.*.public_ip]
-# }
+output "BastionPublicIP" {
+  value = [oci_core_instance.Bastion.*.public_ip]
+}
 
 output "VcnID" {
   value = [oci_core_virtual_network.VCN.id]
